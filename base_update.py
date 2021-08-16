e0e4e28e42b314261913eecb548f55c1f9083347
joemulberry <emailmeit@protonmail.com>
2021-08-16 16:12:24 +0100

    initial submition

---
+++ base_update.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+dicts = []
+
+for cardID in range(1,400):
+    
+    url = 'https://parallel.life/cards/' + str(cardID) + '/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    if len(soup.select('h3')) > 0:
+        name = soup.select('title')
+        h3s = soup.select('h3')
+        supply_p = soup.select('p')[2].text
+
+        h2s = soup.select('h2')        
+        
+        d = {'name': name[0].text.lower().replace('parallel masterpiece // alpha // ','').strip(),
+            'parallel': h3s[0].text,
+            'description': h3s[1].text,
+            'rarity': supply_p.split(' // ')[0].strip(),
+            'supply': int(supply_p.split('// Edition of ')[1].strip()),
+            'parallel_id': str(cardID)
+            }
+
+        links = soup.select('a')
+        for link in links: 
+            if 'opensea' in link['href']:
+                d['opensea_id'] = link['href'].replace(
+                    'https://opensea.io/assets/0x76be3b62873462d2142405439777e971754e8e77/', '')
+                
+        if len(h3s) > 3:
+            d['artist'] = h3s[2].text.split('// @')[1].strip()
+        else:
+            d['artist'] = ''
+        
+        if 'Masterpieces' in d['description']:
+            d['type'] = 'masterpiece'
+        elif 'Card Back' in d['description']:
+            d['type'] = 'card back'
+        elif 'concept art' in d['name']:
+            d['type'] = 'concept art'
+        else:
+            d['type'] = 'card'
+
+        if '[se]' in d['name']:
+            d['standard'] = 'se'
+        else:
+            d['standard'] = 'standard'
+
+        dicts.append(d)
+
+with open('core.json', 'w') as fout:
+    json.dump(dicts, fout)
